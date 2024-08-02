60050fceac716bbd6b6c530f9349ae39dfed981b
xyclose11 <harrisgus1@hotmail.com>
2024-08-02 13:25:13 -0500

    Created basic CLI

--- src/ToDoApp.java
+++ src/ToDoApp.java
@@ -1,38 +1,90 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ToDoApp {
     public static void main(String[] args) {
+        int userCont = -1;
+        int loopCtr = -1;
+        int userChoice;
+        Scanner userInput = new Scanner(System.in);
 
-        todoItem myItem1 = new todoItem();
-        myItem1.setName("Example Todo Item");
+        while (userCont != 0) {
+            loopCtr++;
+            // Continue the program
+            if (loopCtr == 0) {
+                System.out.println("Hello User!, welcome to UETodo List!");
+            }
 
-        todoItem myItem2 = new todoItem();
-        myItem2.setName("Second Item");
+            System.out.println("""
+                        Please enter any number from the following list to continue:
+                        1. View Todo list
+                        2. Add new Todo item
+                        3. Edit a todo item
+                        4. Remove a todo item
+                        
+                        0. Exit program (ALL DATA WILL BE LOST!!)
+                        """);
+            userChoice = userInput.nextInt();
 
-        todoItem myItem3 = new todoItem();
-        myItem3.setName("Third Item");
+            switch (userChoice) {
+                // View List
+                case 1:
+                    break;
 
-        arrayList<todoItem> myList = new arrayList<todoItem>(5);
+                // Create new item
+                case 2:
+                    break;
 
-        myList.append(myItem1);
-        myList.append(myItem1);
-        myList.append(myItem1);
-        myList.append(myItem1);
-        myList.append(myItem1);
-        myList.append(myItem1);
-        myList.append(myItem1);
-        myList.prepend(myItem2);
+                // Edit existing item
+                case 3:
+                    break;
 
-        myList.remove(myItem1);
-        myList.insertAt(7, myItem3);
+                // Remove item
+                case 4:
+                    break;
 
+                // Exit program
+                case 0:
+                    userCont = 0;
+                    break;
+            }
 
-        System.out.println(myList.capacity);
-        System.out.println(myList.length);
-
-        for (int i = 0; i < myList.length ; i++) {
-            todoItem temp = myList.get(i);
-            System.out.println(temp.name);
         }
+
+
+
+
+
+//        todoItem myItem1 = new todoItem();
+//        myItem1.setName("Example Todo Item");
+//
+//        todoItem myItem2 = new todoItem();
+//        myItem2.setName("Second Item");
+//
+//        todoItem myItem3 = new todoItem();
+//        myItem3.setName("Third Item");
+//
+//        arrayList<todoItem> myList = new arrayList<todoItem>(5);
+//
+//        myList.append(myItem1);
+//        myList.append(myItem1);
+//        myList.append(myItem1);
+//        myList.append(myItem1);
+//        myList.append(myItem1);
+//        myList.append(myItem1);
+//        myList.append(myItem1);
+//        myList.prepend(myItem2);
+//
+//        myList.remove(myItem1);
+//        myList.insertAt(7, myItem3);
+//
+//
+//        System.out.println(myList.capacity);
+//        System.out.println(myList.length);
+//
+//        for (int i = 0; i < myList.length ; i++) {
+//            todoItem temp = myList.get(i);
+//            System.out.println(temp.name);
+//        }
     }
 }
